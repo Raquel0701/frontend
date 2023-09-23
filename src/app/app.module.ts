d21d8faf43d02275029547825847b7c52bf3fb2e
Raquel0701 <diana.diaz7196@gmail.com>
2023-09-23 14:56:01 -0500

    add componete list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { HttpClientModule } from '@angular/common/http';
+// Importa todos los iconos de Bootstrap Icons
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListaUsuariosComponent,
-    PerfilComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ListaUsuariosComponent, PerfilComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
